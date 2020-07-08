a0b2618a88f7d22b9739dacb6dd9e1a0bb6df0cb
LoveFromSea <FGcht123>
2020-07-08 22:39:47 +0800

    翻译可选，基本翻译完美功能了

--- app/src/main/java/com/example/words/Fragment/BFragment.java
+++ app/src/main/java/com/example/words/Fragment/BFragment.java
@@ -12,8 +12,6 @@ import android.widget.*;
 import androidx.annotation.Nullable;
 import com.example.words.Adapter.MyAdapter;
 import com.example.words.Bean._words;
-import com.example.words.Adapter.MyAdapter;
-import com.example.words.Bean._words;
 import com.example.words.Pages.Select_word;
 import com.example.words.R;
 import com.example.words.TransImpl.TransApi;
@@ -39,45 +37,31 @@ public class BFragment extends androidx.fragment.app.Fragment {
     private TextView resultTextView;
     private Handler handler = new Handler();
     private EditText myEdit;
+    private Spinner langSpinner;
+    private String changedLang;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_b, container, false);
-        myEdit = (EditText)view.findViewById(R.id.edit_word);
-        searchBtn = (ImageButton)view.findViewById(R.id.search_btn);
-        resultTextView = (TextView)view.findViewById(R.id.result);
-        searchBtn.setOnClickListener(new View.OnClickListener() {
+        final View view = inflater.inflate(R.layout.fragment_b, container, false);
+        myEdit = (EditText) view.findViewById(R.id.edit_word);
+        searchBtn = (ImageButton) view.findViewById(R.id.search_btn);
+        resultTextView = (TextView) view.findViewById(R.id.result);
+        langSpinner = (Spinner) view.findViewById(R.id.sp_major);
+        langSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+                String[] languages = getResources().getStringArray(R.array.languages);
+                changedLang = languages[pos];
+            }
 
-                final String query = myEdit.getText().toString();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        String resultJson = new TransApi().getTransResult(query, "auto", "zh");
-                        Log.i("Tran Result============================", resultJson);
-                        //拿到结果，对结果进行解析。
-                        Gson gson = new Gson();
-                        TranslateResult translateResult = gson.fromJson(resultJson, TranslateResult.class);
-                        final List<TranslateResult.TransResultBean> trans_result = translateResult.getTrans_result();
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-
-                                String dst = "";
-                                for (TranslateResult.TransResultBean s : trans_result
-                                ) {
-                                    dst = dst + "\n" + s.getDst();
-                                }
-                                resultTextView.setText(dst);
-                            }
-                        });
-
-                    }
-                }).start();
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
             }
         });
+
         initView();
         return view;
     }
@@ -98,8 +82,45 @@ public class BFragment extends androidx.fragment.app.Fragment {
     }
 
     private void initView() {
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String query = myEdit.getText().toString();
+                if (query.equals("") || query == null) {
+                    Toast.makeText(getActivity(), "请输入要翻译的词", Toast.LENGTH_SHORT).show();
+                } else {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String toTrans = "zh";
+                            if (changedLang.substring(changedLang.length() - 1).equals("中")) {
+                                toTrans = "zh";
+                            } else if (changedLang.substring(changedLang.length() - 1).equals("英")) {
+                                toTrans = "en";
+                            }
+                            final String finalToTrans = toTrans;
+                            String resultJson = new TransApi().getTransResult(query, "auto", finalToTrans);
+                            Log.i("Tran Result============================", resultJson);
+                            //拿到结果，对结果进行解析。
+                            Gson gson = new Gson();
+                            TranslateResult translateResult = gson.fromJson(resultJson, TranslateResult.class);
+                            final List<TranslateResult.TransResultBean> trans_result = translateResult.getTrans_result();
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    String dst = "";
+                                    for (TranslateResult.TransResultBean s : trans_result
+                                    ) {
+                                        dst = dst + "\n" + s.getDst();
+                                    }
+                                    resultTextView.setText(dst);
+                                }
+                            });
 
-
-
+                        }
+                    }).start();
+                }
+            }
+        });
     }
 }
